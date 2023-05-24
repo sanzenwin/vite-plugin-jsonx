14428d83830e18be8274b76fedf2dc23a616cbf9
Gokhan Taskan <gokhan.frontfeed@gmail.com>
2023-05-24 17:35:52 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import fs from "fs";
-//@ts-ignore
-import { safe as jsonc } from "jsonc";
-import type { IParseOptions, Reviver } from "jsonc/lib/interfaces";
+import { jsonc } from "jsonc";
+import { IParseOptions, Reviver } from "jsonc/lib/interfaces";
 import { createRequire } from "module";
 import type { Plugin } from "vite";
 
@@ -26,6 +25,8 @@ const vitePluginJsonx = (
       if (id.endsWith(".json5") || id.endsWith(".jsonc")) {
         return id; // this signals that we want to resolve this module ourselves in `load()`
       }
+
+      return null; // otherwise let other plugins handle it
     },
 
     async load(id: string) {
@@ -38,13 +39,15 @@ const vitePluginJsonx = (
 
         if (id.endsWith(".jsonc")) {
           const content = fs.readFileSync(id, "utf-8");
-          const [err, data] = jsonc.parse(content, jsoncParseOptions);
+          const [err, data] = jsonc.safe.parse(content, jsoncParseOptions);
           if (err) throw new Error(err.message);
           return `export default ${JSON.stringify(data)};`;
         }
-      } catch (error: any) {
-        throw new Error(`Error while parsing ${id}: ${error.message}`);
+      } catch (error) {
+        throw new Error(`Error while parsing ${id}: ${(error as any).message}`);
       }
+
+      return null; // other ids should be handled as usually
     },
   };
 };
