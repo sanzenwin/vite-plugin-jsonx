3a2df5c77ae4e5cf197e3bbcf67ace31818e25ec
sanzenwin <sanzenwin@gmail.com>
2024-06-13 16:26:18 +0800

    v1.1.2001

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
 import fs from "fs";
-import { jsonc } from "jsonc";
+import {jsonc} from "jsonc";
 import json5 from "json5";
-import { Plugin } from "vite";
+import {Plugin} from "vite";
+
+interface Map {
+  [key: string]: string;
+}
 
 interface PluginOptions {
   json5ParserOptions?: Parameters<typeof json5.parse>[1];
   jsoncParserOptions?: Parameters<typeof jsonc.safe.parse>[1];
+  map?: Map;
+}
+
+const mapDefault: Map = {
+  'json5': 'json5',
+  'jsonc': 'jsonc'
+}
+
+const typeList = Object.values(mapDefault)
+
+function getExt(name: string) {
+  return name.slice((Math.max(0, name.lastIndexOf(".")) || Infinity) + 1)
+}
+
+function getType(map: Map | undefined, name: string) {
+  const mapFinal: Map = {...mapDefault, ...map}
+  return mapFinal[getExt(name)]
 }
 
 /**
@@ -14,30 +35,25 @@ interface PluginOptions {
  * @param {object} options.json5ParserOptions - More details: https://github.com/json5/json5#json5parse
  * @param {object} options.jsoncParserOptions - More details: https://onury.io/jsonc//api#jsonc.safe.parse
  */
-export const jsonX = ({
-  json5ParserOptions,
-  jsoncParserOptions,
-}: PluginOptions = {}): Plugin => {
+export const jsonX = ({json5ParserOptions, jsoncParserOptions, map}: PluginOptions = {}): Plugin => {
   return {
     name: "vite-plugin-jsonx", // name of the plugin
 
     resolveId(id: string) {
-      if (id.endsWith(".json5") || id.endsWith(".jsonc")) {
+      if (typeList.includes(getType(map, id))) {
         return id; // this signals that we want to resolve this module ourselves in `load()`
       }
-
       return null; // otherwise let other plugins handle it
     },
 
     async load(id: string) {
+      const type = getType(map, id)
       try {
-        if (id.endsWith(".json5")) {
+        if (type === "json5") {
           const content = fs.readFileSync(id, "utf-8");
           const data = json5.parse(content, json5ParserOptions);
           return `export default ${JSON.stringify(data)};`;
-        }
-
-        if (id.endsWith(".jsonc")) {
+        } else if (type === "jsonc") {
           const content = fs.readFileSync(id, "utf-8");
           const [err, data] = jsonc.safe.parse(content, jsoncParserOptions);
           if (err) throw new Error(err.message);
